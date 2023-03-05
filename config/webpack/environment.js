e38b82cdbaa967900f41d639725a6056e371962c
825814 <120297399+825814@users.noreply.github.com>
2023-03-05 02:03:19 +0000

    [update]Bootstrapスペルミスだった

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,13 +3,13 @@ const { environment } = require('@rails/webpacker')
 module.exports = environment
 
 const webpack = require('webpack')
-environment.plugins.parend(
+environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'
   })
-  )
+)
 
 
